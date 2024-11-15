87976232d22f9ce313f4f2b9e074a3bf3080f3f0
vendyggod <charli98@mail.ru>
2024-11-15 12:15:14 +0300

    fix: try-catch block for async function getAddress

--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,9 +1,14 @@
 export async function getAddress({ latitude, longitude }) {
-  const res = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
-  );
-  if (!res.ok) throw Error("Failed getting address");
+  try {
+    const res = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
+    );
+    if (!res.ok) throw Error("Failed getting address");
 
-  const data = await res.json();
-  return data;
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(`Error fetching address: ${err.message}`);
+    return null;
+  }
 }
