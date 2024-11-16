0352387129442f645a2f128e6da6ba8c86b4e5de
vendyggod <charli98@mail.ru>
2024-11-17 01:24:50 +0300

    feat: show pizza ingredients

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,16 +1,21 @@
 import { formatCurrency } from "../../utils/helpers";
 
-function OrderItem({ item, isLoadingIngredients, ingredients }) {
+function OrderItem({ item, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
     <li className="py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
-        <p>
-          <span className="font-bold">{quantity}&times;</span> {name}
+        <p className="flex flex-col">
+          <span className="font-bold">
+            {quantity}&times; {name}
+          </span>
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {ingredients?.join(", ") || "Loading..."}
+      </p>
     </li>
   );
 }
