e278ba7a94fb800e6dcd48b013e5ad0b37ac9fef
vendyggod <charli98@mail.ru>
2024-11-18 12:59:57 +0300

    feat: add update order action

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ui/Home.jsx";
-import Error from "./ui/Error.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
-import Cart from "./features/cart/Cart.jsx";
 import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder.jsx";
+import { action as updateOrderAction } from "./features/order/UpdateOrder.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
+import Home from "./ui/Home.jsx";
+import Error from "./ui/Error.jsx";
+import Cart from "./features/cart/Cart.jsx";
 
 const router = createBrowserRouter([
   {
@@ -33,6 +34,7 @@ const router = createBrowserRouter([
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        action: updateOrderAction,
         errorElement: <Error />,
       },
     ],
