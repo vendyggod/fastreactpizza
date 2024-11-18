cd8bdf9f55c0b9f890c5073d9c4847ce0d917adc
vendyggod <charli98@mail.ru>
2024-11-18 12:57:44 +0300

    fix: delete unused props

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -2,7 +2,7 @@ import { useFetcher } from "react-router-dom";
 import { updateOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 
-function UpdateOrder({ order }) {
+function UpdateOrder() {
   const fetcher = useFetcher();
 
   return (
